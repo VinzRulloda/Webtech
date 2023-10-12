795478b4b3e8287a80ff339d48f5fc515f8af0c1
ClarenzYumul <147524868+ClarenzYumul@users.noreply.github.com>
2023-10-12 22:33:28 +0800

    tried parsing the xml file (needs to be updated)

--- ACTIVITY 1/assets/javascripts/main.js
+++ ACTIVITY 1/assets/javascripts/main.js
@@ -1,7 +1,25 @@
-<script>
-// When the user clicks on div, open the popup
-function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
-}
-</script>
+document.addEventListener("DOMContentLoaded", function() {
+  
+  fetch("ACTIVITY 1\assets\Recipes.xml") 
+      .then(response => response.text())
+      .then(data => {
+          
+          const parser = new DOMParser();
+          const xmlDoc = parser.parseFromString(data, "text/xml");
+          const dishElements = xmlDoc.querySelectorAll("Dish");
+
+          
+          const recipesContainer = document.getElementById("recipes-container");
+
+         
+          dishElements.forEach(dish => {
+              const dishName = dish.querySelector("Name").textContent;
+
+              
+              const dishElement = document.createElement("p");
+              dishElement.textContent = dishName;
+              recipesContainer.appendChild(dishElement);
+          });
+      })
+      .catch(error => console.error("Error fetching or parsing XML:", error));
+});
