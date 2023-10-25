94a93547dd4c892e27f9325f71458b95860f27df
VinzRulloda <2213505@slu.edu.ph>
2023-10-25 13:18:14 +0800

    added map on services

--- ACTIVITY 2/assets/javascript/script.js
+++ ACTIVITY 2/assets/javascript/script.js
@@ -21,3 +21,18 @@ function showSlides(n) {
 
     slides[slideIndex - 1].style.display = 'block';
 }
+
+// https://leafletjs.com/reference.html#marker-option (documentation for reference)
+//map
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+.bindPopup('you are here')
+.openPopup();
+maxZoom: 19
+.addTo(map);
+
