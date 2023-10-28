43715203ba18beee819093fdc0bfddee3ec54c4f
VinzRulloda <2213505@slu.edu.ph>
2023-10-28 10:06:31 +0800

    updated js file

--- ACTIVITY 2/assets/javascript/map.js
+++ ACTIVITY 2/assets/javascript/map.js
@@ -149,23 +149,23 @@ const destinationParam = getDestinationFromQuery();
 if (destinationParam) {
     // Replace these coordinates with the actual coordinates for your destinations
     if (destinationParam === 'burnham') {
-        setDestinationOnMap('Burnham Park', 16.4023, 120.5935);
+        setDestinationOnMap('Burnham Park', 16.412478, 120.593956);
     } else if (destinationParam === 'cathedral') {
-        setDestinationOnMap('Baguio Cathedral', 16.4023, 120.5960);
+        setDestinationOnMap('Baguio Cathedral', 16.412856, 120.598470);
     } else if (destinationParam === 'botanical') {
-        setDestinationOnMap('Botanical Garden', 16.4023, 120.6014);
+        setDestinationOnMap('Botanical Garden', 16.414699, 120.613445);
     }
 }
 
 // Add event listeners to the buttons to set the destination
 document.getElementById('burnham').addEventListener('click', function () {
-    setDestinationOnMap('Burnham Park', 16.4023, 120.5935);
+    setDestinationOnMap('Burnham Park', 16.412478, 120.593956);
 });
 
 document.getElementById('cathedral').addEventListener('click', function () {
-    setDestinationOnMap('Baguio Cathedral', 16.4023, 120.5960);
+    setDestinationOnMap('Baguio Cathedral', 16.412856, 120.598470);
 });
 
 document.getElementById('botanical').addEventListener('click', function () {
-    setDestinationOnMap('Botanical Garden', 16.4023, 120.6014);
+    setDestinationOnMap('Botanical Garden', 16.414699, 120.613445);
 });
