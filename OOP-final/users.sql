1d858b722ec5b11e68ef6070f8a9ec7c4fcd1872
Vinz Rulloda <102574744+DaVinzci@users.noreply.github.com>
2023-12-04 23:51:55 +0800

    import SQL

---
+++ OOP-final/users.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 04, 2023 at 03:51 PM
+-- Server version: 8.2.0
+-- PHP Version: 8.2.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `users`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `acc`
+--
+
+DROP TABLE IF EXISTS `acc`;
+CREATE TABLE IF NOT EXISTS `acc` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `usertype` varchar(50) NOT NULL DEFAULT 'user',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `acc`
+--
+
+INSERT INTO `acc` (`id`, `fname`, `lname`, `username`, `password`, `usertype`) VALUES
+(1, 'Vinz', 'Rulloda', 'user1', 'vinz', 'user'),
+(2, 'OOP', 'Admin', 'admin', '1234', 'admin'),
+(3, 'Clarenz', 'Yumul', 'clarenz', '1234', 'user'),
+(4, 'Christian', 'Duculan', 'c_duculan', '1234', 'user'),
+(5, 'Ivan', 'SanJose', 'ivan', '1234', 'user'),
+(6, 'Meree Iris', 'Lopez', 'iris', '1234', 'user'),
+(7, 'Paula', 'Nicolas', 'paula', '1234', 'user');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
