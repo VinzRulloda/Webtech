091777170ccbc4562a546d3b7fc790365ed078bf
spyro48 <eyevansanjose@gmail.com>
2023-12-12 00:03:33 +0800

    move file

---
+++ FINALEXP/upload_video.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "video";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$title = $_POST['title'];
+$duration = $_POST['duration'];
+$uploaded_by = $_POST['uploaded_by'];
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES['video']['name']);
+
+move_uploaded_file($_FILES['video']['tmp_name'], $target_file);
+
+$sql = "INSERT INTO uploads (title, duration, uploaded_by) VALUES ('$title', $duration, '$uploaded_by')";
+if ($conn->query($sql) === TRUE) {
+    echo "Video uploaded successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
