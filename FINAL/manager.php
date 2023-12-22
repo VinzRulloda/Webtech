37bde8e479fccc7c96de21486750e9a1f01e4a37
paulap <2144792@slu.edu.ph>
2023-12-22 12:30:13 +0800

    Fix interface for upload

--- FINAL/manager.php
+++ FINAL/manager.php
@@ -8,8 +8,7 @@ require 'db_connection.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/styles.css">
-    <script src="assets/javascript/manager-script.js"></script>
-
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <title>OOP-Final Manager</title>
 </head>
 <body>
@@ -30,64 +29,91 @@ require 'db_connection.php';
     </nav>
 
     <main>
-        <div class="popup-container" id="uploadPopup">
+        <!-- <div class="popup-container" id="uploadPopup">
             <div id="popupContent"></div>
-        </div>
-
-        <div class="m-container">
-        <video type="video/mp4" id="vidPlayer" allow="autoplay" id="music" autoplay muted>
-        </video>
-
-                
-              
-        </div>
-
-        <div class="container-right">
-            <h2>Arrangement Content</h2> 
-            
-            <form action="upload.php" method="post" enctype="multipart/form-data">
-                    <label>Video Title:</label>
-                    <input type="text" name="title" id="videotitle" required>
-                <div class="button-container"> 
-                    
-                    <input type="file" name="files[]" id="videoInput" required multiple>
-                    <button name="submit" type="submit">Add</button>
+        </div> -->
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <video type="video/mp4" id="vidPlayer" allow="autoplay" id="music" autoplay muted>
+                    </video>
                 </div>
-            </form>
-            <table class="data-table" id="videoTable">
-            <tr>
-                <th>ID</th>
-                <th>Title</th>
-                <th>Uploaded By</th>
-                <th>Duration</th>
-                <th>File path</th>
-                <th>Action</th>
-            </tr>
-            <?php
-                $stmt = $pdo->query('SELECT * FROM uploads');
-                $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                foreach ($rows as $row) {
-                    echo '<tr>';
-                    echo '<td>' . $row['id'] . '</td>';
-                    echo '<td>' . $row['title'] . '</td>';
-                    echo '<td>' . $row['uploaded_by'] . '</td>';
-                    echo '<td>' . $row['duration'] . '</td>';
-                    echo '<td>' . $row['file_path'] . '</td>';
-                    echo '<td>
-                            <button onclick="remove_video('.$row['id'].')">Remove</button>
-                          </td>';
-                    echo '</tr>';
-                }
-            ?>
-             
-            </table>
+                <div class="col">
+                    <h2>Arrangement Content</h2> 
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addVideoModal">
+                        Add
+                    </button>
+                    <table class="table data-table" id="videoTable">
+                        <tr>
+                            <th>ID</th>
+                            <th>Title</th>
+                            <th>Uploaded By</th>
+                            <th>Duration</th>
+                            <th>File path</th>
+                            <th>Action</th>
+                        </tr>
+                        <?php
+                            $stmt = $pdo->query('SELECT * FROM uploads');
+                            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                            foreach ($rows as $row) {
+                                echo '<tr>';
+                                echo '<td>' . $row['id'] . '</td>';
+                                echo '<td>' . $row['title'] . '</td>';
+                                echo '<td>' . $row['uploaded_by'] . '</td>';
+                                echo '<td>' . $row['duration'] . '</td>';
+                                echo '<td>' . $row['file_path'] . '</td>';
+                                echo '<td>
+                                        <button class="btn btn-danger btn-sm" onclick="remove_video('.$row['id'].')">Remove</button>
+                                    </td>';
+                                echo '</tr>';
+                            }
+                        ?>
+                        
+                        </table>
+                </div>
+                </div>
+            </div>
         </div>
     </main>
+
+     
+    <!-- Modal -->
+    <div class="modal fade" id="addVideoModal" tabindex="-1" role="dialog" aria-labelledby="addVideoModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addVideoModalLabel">Add Video</h5>
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="uploadVideoForm" action="upload.php" method="post" enctype="multipart/form-data">
     
-    <footer>
-    <img src="assets/images/EDITED-FOOTER.png" id="slufooter">
-   
-    </footer>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="inputGroup-sizing-sm">Title:</span>
+                        <input class="form-control" type="text" name="title" id="videotitle" required>
+                    </div>
+                    <div class="input-group mb-3"> 
+                        <input class="form-control" type="file" name="files[]" id="videoInput" required multiple>
+                    </div>
+                </form>     
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button form="uploadVideoForm" type="submit" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+    </div>
+    
+
 </body>
+<footer>
+    <img src="assets/images/EDITED-FOOTER.png" id="slufooter">
+    <script src="assets/javascript/manager-script.js"></script>
+    <script src="assets/javascript/bootstrap.min.js"></script>
+</footer>
 </html>
